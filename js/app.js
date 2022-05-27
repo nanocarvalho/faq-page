fb5fde58d08c8fa5b426db57d3202642e07ec487
Nano <nanoc.contato@gmail.com>
2022-05-27 17:26:59 -0300

    Major changes to the way that I did the faq container, changed to an ul with lis

--- js/app.js
+++ js/app.js
@@ -5,12 +5,9 @@ const sectionOne = document.querySelector('[data-js="section-1"]')
 const sectionTwo = document.querySelector('[data-js="section-2"]')
 
 const faqTitleContainer = Array.from(document.querySelectorAll('.faq-title'))
-const faqTitleAnchor = document.querySelectorAll('.faq-title > a')
-
-//There is a bug, because the way I did the title of the faq, the <a> tag is called no listener. So basically I need to redo everything haha. I'll leave to another time.
 
 const openAccordion = event =>  {
-    const text = event.target.querySelector('.faq-text')
+    const text = event.target.parentNode.querySelector('.faq-text')
     const arrowIcon = event.target.querySelector('img')
     arrowIcon.src = "./img/arrow-up.svg"
     text.classList.toggle('active')
